6050d0e69e95b45a799f6d29abe10cfdd5b5b23e
Ivan Karagyozov <ivankaragyozov0@gmail.com>
2022-06-15 18:56:33 +0300

    Exercises tests and small changes

--- 07_UnitTestingAndErrorHandling/lectures/test/demo.test.js
+++ 07_UnitTestingAndErrorHandling/lectures/test/demo.test.js
@@ -1,6 +1,10 @@
 const {expect} = require('chai');
 const demo = require('./demo');
 
+/**
+ * Start through console with: {test file directory} mocha {test file name.js}
+ */
+
 describe('even or odd', function () {
     it('isString', function () {
         expect(demo(3)).to.undefined;
